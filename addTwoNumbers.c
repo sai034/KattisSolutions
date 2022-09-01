3e4665acf9825085490a0f8e5170f309f45951e6
sai034 <112326008+sai034@users.noreply.github.com>
2022-09-01 23:17:24 +0530

    Create addTwoNumbers.c

---
+++ addTwoNumbers.c
@@ -0,0 +1,11 @@
+#	Add Two Numbers
+#https://open.kattis.com/problems/addtwonumbers
+
+#include<stdio.h>
+int main(){
+int a,b,sum;
+scanf("%d %d",&a,&b);
+sum=a+b;
+printf("%d",sum);
+return 0;    
+}
