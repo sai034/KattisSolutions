3b736f7a6ba57524e122bc87526e9ad8d4e0f1b9
sai034 <112326008+sai034@users.noreply.github.com>
2022-09-02 21:24:15 +0530

    Create stuckInATimeLoop.c

---
+++ stuckInATimeLoop.c
@@ -0,0 +1,12 @@
+#	Stuck In A Time Loop
+#https://open.kattis.com/problems/timeloop
+
+#include<stdio.h>
+int main(){
+ int i,N;
+ scanf("%d",&N);
+ for(i=1;i<=N;i++){
+printf("%d Abracadabra\n",i);
+ }
+return 0;
+}
