2ed550eec787fb307cc1fd532d0e8bee857aeee6
sai034 <112326008+sai034@users.noreply.github.com>
2022-09-02 21:22:33 +0530

    Create quadrantSelection.c

---
+++ quadrantSelection.c
@@ -0,0 +1,17 @@
+#	Quadrant Selection
+#https://open.kattis.com/problems/quadrant
+
+#include<stdio.h>
+int main(){
+int x,y;
+scanf("%d %d",&x,&y);
+if(x>0&&y>0)
+  printf("1");
+else if(x<0&&y>0)
+    printf("2");
+else if(x<0&&y<0)
+printf("3");
+else if(x>0&&y<0)
+   printf("4");
+return 0;
+}
