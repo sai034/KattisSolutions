540be0e3695b444ac28b91cdbca1b88dd5af8457
sai034 <112326008+sai034@users.noreply.github.com>
2022-09-02 21:27:36 +0530

    Create takeTwoStones.c

---
+++ takeTwoStones.c
@@ -0,0 +1,13 @@
+#	Take Two Stones
+#https://open.kattis.com/problems/twostones
+
+#include<stdio.h>
+int main(){
+int N;
+scanf("%d",&N);
+if (N%2==0)
+ printf("Bob");
+else
+  printf("Alice");
+return 0;
+}
