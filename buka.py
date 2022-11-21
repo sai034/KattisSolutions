c898aa543baf877cf60dde925beb148df8057cac
sai034 <112326008+sai034@users.noreply.github.com>
2022-11-21 19:37:14 +0530

    Create buka.py

---
+++ buka.py
@@ -0,0 +1,14 @@
+#	Buka
+#https://open.kattis.com/problems/buka
+
+a=int(input())
+b=str(input())
+c=int(input())
+if b=='+':
+    print(a+c)
+if b=='-':
+    print(a-c)
+if b=="*":
+    print(a*c)
+if b=='/':
+    print(a/c)
