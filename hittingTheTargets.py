e852c5bdf0c8012dd5050c8b1b586e10c448689b
sai034 <112326008+sai034@users.noreply.github.com>
2023-02-10 17:51:47 +0530

    Update hittingTheTargets.py

--- hittingTheTargets.py
+++ hittingTheTargets.py
@@ -1,3 +1,28 @@
 #Hitting the Targets
 #https://open.kattis.com/problems/hittingtargets
 
+import math
+n = int(input())
+r = list()
+c = list()
+for i in range(n):
+    form, *d = input().split()
+    d = list(map(int, d))
+    if form == "rectangle":
+        r.append(d)
+    else:
+        c.append(d)
+ns = int(input())
+for i in range(ns):
+    x, y = list(map(int, input().split()))
+    nh = 0
+    for x1, y1, x2, y2 in r:
+        if x1 <= x <= x2 and y1 <= y <= y2:
+            nh = nh + 1
+    for x1, y1, rad in c:
+        X = x1 - x
+        Y = y1 - y
+        dis = math.sqrt(X ** 2 + Y ** 2)
+        if dis <= rad:
+            nh = nh + 1
+    print(nh)
