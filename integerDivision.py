3996b40991275d9029fbd218669bfecf74c4a320
sai034 <112326008+sai034@users.noreply.github.com>
2023-02-10 17:54:58 +0530

    Update integerDivision.py

--- integerDivision.py
+++ integerDivision.py
@@ -1,4 +1,9 @@
 #Integer Division
 #https://open.kattis.com/problems/integerdivision
 
-
+from collections import defaultdict
+n=defaultdict(lambda:0)
+d=int(input().split()[1])
+for a in map(int,input().split()):
+    n[a//d]+=1
+print(sum((a*(a-1))//2 for a in n.values()))
