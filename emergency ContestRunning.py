d29b4590303e959ed75454bc7d336aec6ab6671d
sai034 <112326008+sai034@users.noreply.github.com>
2023-05-09 12:26:37 +0530

    Create emergency ContestRunning.py

---
+++ emergency ContestRunning.py
@@ -0,0 +1,8 @@
+#	Emergency Contest Running
+#https://open.kattis.com/problems/emergency
+
+a,b=map(int,input().split())
+if b>=a:
+    print(a-1)
+else:
+    print(b + int(b + (a - 1) % b!= a - 1) + (a - 1) % b)
